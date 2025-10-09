aa70bb6d54e7ead63cdb0d1f3712a2360b96eb2a
AuliaDiva1 <161435778+AuliaDiva1@users.noreply.github.com>
2025-10-09 14:35:05 +0700

    Update 20250924031819_create_master_agama.js

--- be-express-siakad/src/migrations/20250924031819_create_master_agama.js
+++ be-express-siakad/src/migrations/20250924031819_create_master_agama.js
@@ -2,20 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-module.exports.up = function(knex) {
-  return knex.schema.createTable('master_info_sekolah', function(table) {
+module.export.up = function(knex) {
+  return knex.schema.createTable('master_agama', function(table) {
     table.increments('id').primary(); // Primary Key, Auto Increment
-    table.string('ALAMAT', 255).notNullable(); // Alamat sekolah
-    table.string('JENJANG_AKREDITASI', 100); // Jenjang Akreditasi
-    table.string('NAMA_SEKOLAH', 255).notNullable(); // Nama Sekolah
-    table.string('NPSN', 20).notNullable(); // Nomor Pokok Sekolah Nasional
-    table.string('STATUS', 50); // Status sekolah
-    table.timestamp('TANGGAL_AKREDITASI').defaultTo(knex.fn.now()); // Tanggal Akreditasi
+    table.string('NAMAAGAMA', 100).notNullable(); // Nama agama
     table.timestamp('created_at').defaultTo(knex.fn.now()); // Tanggal dibuat
     table.timestamp('updated_at').defaultTo(knex.fn.now()); // Tanggal diupdate
   });
 };
 
-module.exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('master_info_sekolah');
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+module.export.down = function(knex) {
+  return knex.schema.dropTableIfExists('master_agama');  // Menghapus tabel jika rollback
 };
